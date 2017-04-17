d34bb91114527bbf2cdb9fe574cf97c31aa9342c
koox00 <cooxlee@gmail.com>
2017-04-17 03:18:49 +0300

    Change post id to str

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=200)
     title = models.CharField(max_length=200)
     url = models.URLField()
     body = models.TextField()
